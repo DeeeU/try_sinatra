5b802b6d24ad5476917718dc28464628b8331895
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-06-15 09:20:27 +0900

    [add]ルーティング追加

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # try_sinatra
+
+立ち上げ方
+bundle exec ruby app.rb
--- app.rb
+++ app.rb
@@ -1,5 +1,39 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
 
-get '/' do
-  'hello'
+class Memo
+  def initialize()
+    @id = null
+    @title = null
+    @text = null
+  end
 end
+get "/" do
+  erb :index
+end
+
+get"/create" do
+  erb :create
+end
+
+get "/memoes/:id" do |name|
+  memo
+  erb :detail
+end
+
+# 入力したデータがjsonファイル(database/data.json)にプッシュされるはず....
+post "/confirm" do
+  @title = params[:title]
+  @text = params[:text]
+  File.open("database/data.json", "a") do |w|
+    data = {"ID" => 1, "title" => @title, "text" => @text}
+    w.write(data.to_json)
+  end
+  redirect to('/')
+end
+
+#　editの時に使うpatch処理
+# patch "/edit/*" do
+
+# end
