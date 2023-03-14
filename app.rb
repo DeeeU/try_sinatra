5d8bedae246493abdd153096f204ad2febb01000
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-03-14 10:16:16 +0900

    change: 接続回数の削減

--- app.rb
+++ app.rb
@@ -16,26 +16,25 @@ end
 
 class Memo
   def self.create(title: title(), text: text())
-    query = 'INSERT INTO memoes(title, text) VALUES ($1, $2)'
+    query = 'INSERT INTO memos(title, text) VALUES ($1, $2)'
     params = [title, text]
     excute(query, params)
   end
 
   def self.delete(id: id())
-    query = 'DELETE FROM  memoes where id = ($1)'
+    query = 'DELETE FROM  memos where id = ($1)'
     params = [id]
     excute(query, params)
   end
 
   def self.patch(id: id(), title: title(), text: text())
-    query = 'UPDATE memoes SET (title, text) = ($1, $2) where id = ($3)'
+    query = 'UPDATE memos SET (title, text) = ($1, $2) where id = ($3)'
     params = [title, text, id]
     excute(query, params)
   end
 
   def self.excute(query, params)
-    connection = PG::Connection.new(dbname: 'postgres')
-    connection.exec_params(query, params)
+    conn.exec_params(query, params)
   end
 end
 
@@ -49,16 +48,17 @@ def find_memo(data)
   @memo
 end
 
-conn = PG.connect(dbname: 'postgres')
-result = conn.exec('SELECT * FROM memoes')
-conn.close
+def conn
+  @conn ||= PG.connect(dbname: 'db_sinatra')
+end
+
+def read_memos
+  conn.exec('SELECT * FROM memos')
+end
 
 get '/memos' do
   @page_name = 'Top'
-  conn = PG::Connection.new(host: :localhost, dbname: :postgres, port: :'5432')
-  result = conn.exec('SELECT * FROM memoes')
-  conn.close
-  @memos = result
+  @memos = read_memos
   erb :index
 end
 
@@ -69,7 +69,7 @@ end
 
 get '/memos/:id' do
   @page_name = 'Detail'
-  find_memo(result)
+  find_memo(read_memos)
   erb :detail
 end
 
@@ -79,19 +79,19 @@ post '/memos' do
 end
 
 delete '/memos/:id' do
-  find_memo(result)
+  find_memo(read_memos)
   Memo.delete(id: params[:id])
   redirect to('/memos')
 end
 
 get '/memos/:id/edit' do
   @page_name = 'Edit'
-  find_memo(result)
+  find_memo(read_memos)
   erb :edit
 end
 
 patch '/memos/:id' do
-  find_memo(result)
+  find_memo(read_memos)
   Memo.patch(id: params[:id], title: params[:title], text: params[:text])
   redirect to('/memos')
 end
